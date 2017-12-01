9d3ff359bdec9247f2a5d456ce661456a730e4ab
zhangke <zhangke@wezyun.com>
2017-12-01 10:13:02 +0800

    优化controller的切面

--- src/main/java/com/vm/base/aop/ControllerServiceAop.java
+++ src/main/java/com/vm/base/aop/ControllerServiceAop.java
@@ -10,7 +10,26 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import javassist.ClassClassPath;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.Modifier;
+import javassist.NotFoundException;
+import javassist.bytecode.CodeAttribute;
+import javassist.bytecode.LocalVariableAttribute;
+import javassist.bytecode.MethodInfo;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.Map;
 
 @Component
@@ -25,53 +44,37 @@ public class ControllerServiceAop {
     private LoggerWrapper logger = LoggerWrapper.newLoggerWrapper(ControllerServiceAop.class);
 
     @Around("declareJoinPointExpression()")
-    public Object doAroundAdvice(ProceedingJoinPoint proceedingJoinPoint) {
-        logger.info(proceedingJoinPoint.getSignature() + " " + proceedingJoinPoint.getArgs());
-        System.out.println();
-        System.out.println(proceedingJoinPoint.getSignature().getDeclaringTypeName());
-        System.out.println("环绕通知的目标方法名：" + proceedingJoinPoint.getSignature().getName());
+    public Object doAroundAdvice(ProceedingJoinPoint joinPoint) throws ClassNotFoundException, NotFoundException {
+        StringBuffer sb = new StringBuffer(joinPoint.getSignature().toString() +" ==>PARAMS: "+ Arrays.toString(joinPoint.getArgs()));
+
+        logger.info(sb.toString());
+
         Response response = new Response();
         Object data = null;
         try {
-            data = proceedingJoinPoint.proceed();//调用执行目标方法,先執行aop在執行了responsebody
+            data = joinPoint.proceed();//调用执行目标方法,先執行aop在執行了responsebody
             if(data !=null){
                 response.setData((Map<Object, Object>) data);
             }
         } catch (ValidateRuntimeException e) {
-//            logger.error("UsersController#insert error name {} ,password {}, ", name, password);
+            logger.info(sb.toString()+" ==>ERROR: "+e);
             response.setCode(e.getErrorCode().intValue());
             response.setMsg(e.getMessage());
             response.setMsg(e.getMessage());
             e.printStackTrace();
         } catch (Exception e) {
-//            logger.error("UsersController#insert error name {} ,password {}, ", name, password);
+            logger.info(sb.toString()+" ==>ERROR: "+e);
             response.setCode(VMRuntimeException.ErrorCode.UNKNOWN.getCode().intValue());
             response.setMsg(VMRuntimeException.ErrorCode.UNKNOWN.getMsg());
         } catch (Throwable throwable) {
             throwable.printStackTrace();
+            logger.info(sb.toString()+" ,error:"+throwable.toString());
+            response.setCode(VMRuntimeException.ErrorCode.UNKNOWN.getCode().intValue());
+            response.setMsg(VMRuntimeException.ErrorCode.UNKNOWN.getMsg());
         }
 
         return response;
 
-
-
-//        logger.info("UsersController#insert name {}, password {}", name, password);
-//
-//        try {
-//            service.add(name,password);
-//        } catch (ValidateRuntimeException e) {
-//            logger.error("UsersController#insert error name {} ,password {}, ",name,password);
-//            response.setCode(e.getErrorCode().intValue());
-//            response.setMsg(e.getMessage());
-//            response.setMsg(e.getMessage());
-//            e.printStackTrace();
-//        }catch (Exception e) {
-//            logger.error("UsersController#insert error name {} ,password {}, ",name,password);
-//            response.setCode(VMRuntimeException.ErrorCode.UNKNOWN.getCode().intValue());
-//            response.setMsg(VMRuntimeException.ErrorCode.UNKNOWN.getMsg());
-//        }
-//
-//        logger.info("UsersController#insert response {}", response);
-//        return response;
     }
+
 }
