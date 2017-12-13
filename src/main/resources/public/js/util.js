8f225090d5a07ca40eb5f17e283ec8a4d4551f28
zhangke <zhangke@wezyun.com>
2017-12-13 17:06:49 +0800

    准备制作标签筛选电影功能

--- src/main/resources/public/js/util.js
+++ src/main/resources/public/js/util.js
@@ -1,3 +1,22 @@
+/**
+ * Array.prototype.[method name] allows you to define/overwrite an objects method
+ * needle is the item you are searching for
+ * this is a special variable that refers to "this" instance of an Array.
+ * returns true if needle is in the array, and false otherwise
+ *
+ * example:
+ *  Now you can do things like:
+ *  var x = Array();
+ *  if (x.contains('foo')) {
+ *      do something special
+ *  }
+ */
+Array.prototype.contains = function (needle) {
+    for (i in this) {
+        if (this[i] == needle) return true;
+    }
+    return false;
+}
 
 
 /**
