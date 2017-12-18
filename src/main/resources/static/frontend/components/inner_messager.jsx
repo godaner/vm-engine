a730a576f4a493194ac5c35bb60ac8af9c9273a9
zhangke <zhangke@wezyun.com>
2017-12-18 15:22:23 +0800

    修复inner_msg显示一轮后丢失.

--- src/main/resources/static/frontend/components/inner_messager.jsx
+++ src/main/resources/static/frontend/components/inner_messager.jsx
@@ -5,33 +5,34 @@ var InnerMessager = React.createClass({
     getInitialState: function () {
 
         var state = {};
+        //default msg
         var tip = "正在加载";
         if (this.props.tip != undefined && this.props.tip != "") {
             tip = this.props.tip;
         }
-        state = {tip: tip,cacheTip:"",tipPoint:".",tipPointMaxNum:4,tipPointNum:0,timer:undefined};
-return state;
+        state = {tip: tip, cacheTip: tip, tipPoint: ".", tipPointMaxNum: 4, tipPointNum: 0, timer: undefined};
+        return state;
 
     },
     componentDidMount: function () {
-        this.state.timer = setInterval(function(){
+        this.state.timer = setInterval(function () {
             var state = this.state;
             var tip = state.tip;
-            if(this.state.tipPointNum>=this.state.tipPointMaxNum){
+            if (this.state.tipPointNum >= this.state.tipPointMaxNum) {
                 tip = this.state.cacheTip;
                 state.tipPointNum = 0;
             }
             tip = tip + this.state.tipPoint;
 
-            state.tipPointNum = state.tipPointNum+1;
+            state.tipPointNum = state.tipPointNum + 1;
 
             state.tip = tip;
 
             this.setState(state);
-        }.bind(this),500);
+        }.bind(this), 500);
 
     },
-    componentWillUnmount:function() {
+    componentWillUnmount: function () {
         clearInterval(this.state.timer);
     },
     showMsg(msg) {
@@ -39,7 +40,7 @@ return state;
             msg = "";
 
         }
-        if(msg == ""){
+        if (msg == "") {
             clearInterval(this.state.timer);
         }
         var state = this.state;
