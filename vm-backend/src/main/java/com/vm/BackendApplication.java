551be912b17fdc4de44324304d8724f60400ec41
zhangke <zhangke@wezyun.com>
2018-01-02 11:11:47 +0800

    rebuilding

--- vm-backend/src/main/java/com/vm/BackendApplication.java
+++ vm-backend/src/main/java/com/vm/BackendApplication.java
@@ -1,15 +1,21 @@
-package com.vm.backend;
+package com.vm;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/**
+ * Title:
+ * Description:
+ * Author:zhangke
+ * Date:2017/11/16 14:47
+ */
 @SpringBootApplication
-//@MapperScan(basePackages = "dao")
-public class BootApp {
+@MapperScan(basePackages = "com.vm.dao")
+public class BackendApplication {
     public static void main(String[] args) {
 
-        SpringApplication springApplication = new SpringApplication(BootApp.class);
+        SpringApplication springApplication = new SpringApplication(BackendApplication.class);
 //        springApplication.addListeners(new ApplicationStartedListener());
         springApplication.run(args);
 
