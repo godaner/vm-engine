98840687a7029c794d6fd462d71e641b3a35eb41
zhangke <zhangke@wezyun.com>
2018-01-15 13:19:56 +0800

    frontend antd

---
+++ vm-frontend/src/main/resources/static/node_modules/antd/lib/time-picker/locale/ja_JP.js
@@ -0,0 +1,9 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var locale = {
+    placeholder: '時刻を選択'
+};
+exports['default'] = locale;
---
+++ vm-frontend/src/main/resources/static/node_modules/antd/lib/time-picker/locale/ru_RU.js
@@ -0,0 +1,12 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+/**
+ * Created by Andrey Gayvoronsky on 13/04/16.
+ */
+var locale = {
+  placeholder: 'Выберите время'
+};
+exports['default'] = locale;
