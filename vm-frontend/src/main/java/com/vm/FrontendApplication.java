72f110c12b95c534d57bdd18d802e7a7b44f7f81
zhangke <zhangke@wezyun.com>
2018-01-17 10:31:33 +0800

    perfect

--- vm-frontend/src/main/java/com/vm/FrontendApplication.java
+++ vm-frontend/src/main/java/com/vm/FrontendApplication.java
@@ -4,6 +4,10 @@ import com.vm.frontend.listener.ApplicationStartedListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 
 @SpringBootApplication
@@ -15,5 +19,20 @@ public class FrontendApplication {
         springApplication.addListeners(new ApplicationStartedListener());
         springApplication.run(args);
 
+    }
+    /*********************************** 跨域配置 ********************************/
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
     }
 }
