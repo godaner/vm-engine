3e26da7a2c122324abc61a2ca963632a9abbb9db
zhangke <zhangke@wezyun.com>
2018-01-17 10:00:05 +0800

    rm static source

--- vm-backend/src/main/resources/static/node_modules/antd/es/auto-complete/InputElement.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import React from 'react';
-export default class InputElement extends React.Component<any, any> {
-    private ele;
-    focus: () => void;
-    blur: () => void;
-    saveRef: (ele: HTMLInputElement) => void;
-    render(): React.DetailedReactHTMLElement<any, HTMLElement>;
-}
