c7b4ad580ad1be6c628d0e8459fe5ebac0dc2ef0
zhangke <zhangke@wezyun.com>
2018-01-24 18:19:33 +0800

    to spring cloud

--- vm-eureka/src/main/java/com/vm/EurekaApplication.java
+++ vm-eureka/src/main/java/com/vm/EurekaApplication.java
@@ -3,10 +3,12 @@ package com.vm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
-@SpringBootApplication
 @ServletComponentScan
+@EnableEurekaServer
+@SpringBootApplication
 public class EurekaApplication {
     public static void main(String[] args) {
 
--- vm-provider-user/src/main/java/com/vm/ProviderUserApplication.java
+++ vm-provider-user/src/main/java/com/vm/ProviderUserApplication.java
@@ -3,14 +3,16 @@ package com.vm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
 @ServletComponentScan
-public class UserApplication {
+@EnableDiscoveryClient
+public class ProviderUserApplication {
     public static void main(String[] args) {
 
-        SpringApplication springApplication = new SpringApplication(UserApplication.class);
+        SpringApplication springApplication = new SpringApplication(ProviderUserApplication.class);
         springApplication.run(args);
 
     }
