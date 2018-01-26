3543cd5040a28c8ed3de0b752bfc95c438a3b6ba
zhangke <zhangke@wezyun.com>
2018-01-26 09:37:30 +0800

    config

---
+++ vm-provider-user/src/main/java/com/vm/controller/ConfigClientController.java
@@ -0,0 +1,21 @@
+package com.vm.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by eggyer on 2017/3/13.
+ */
+
+@RestController
+public class ConfigClientController {
+    //取配置文件中的值
+    @Value("${profile}")
+    private String profile;
+
+    @GetMapping("/profile")
+    public String getProfile(){
+        return profile;
+    }
+}
