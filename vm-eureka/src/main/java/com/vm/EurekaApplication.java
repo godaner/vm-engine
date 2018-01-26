83548a796af596a6644e2e090c067eb83bb1cf2d
zhangke <zhangke@wezyun.com>
2018-01-26 14:34:30 +0800

    do

--- vm-eureka/src/main/java/com/vm/EurekaApplication.java
+++ vm-eureka/src/main/java/com/vm/EurekaApplication.java
@@ -3,12 +3,14 @@ package com.vm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
 @EnableEurekaServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EurekaApplication {
     public static void main(String[] args) {
 
