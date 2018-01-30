e9a0b0aaeb466bac55e1dfed44f7699428bb7a32
zhangke <zhangke@wezyun.com>
2018-01-30 13:26:16 +0800

    update config

--- vm-gateway/src/main/java/com/vm/GatewayApplication.java
+++ vm-gateway/src/main/java/com/vm/GatewayApplication.java
@@ -2,10 +2,11 @@ package com.vm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-
+/**
+ * Created by ZhangKe on 2017/12/26.
+ */
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableZuulProxy // 使用@EnableZuulProxy来开启Zuul的支持，如果你不想使用Zuul提供的Filter和反向代理的功能的话，此处可以使用@EnableZuulServer注解
