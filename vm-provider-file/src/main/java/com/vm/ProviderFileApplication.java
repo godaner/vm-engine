6e97e63a23e67073b00cff600fd205d3704d35a7
zhangke <zhangke@wezyun.com>
2018-01-30 09:45:10 +0800

    adjust

--- vm-provider-file/src/main/java/com/vm/ProviderFileApplication.java
+++ vm-provider-file/src/main/java/com/vm/ProviderFileApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @ServletComponentScan
+@EnableDiscoveryClient
 public class ProviderFileApplication {
     public static void main(String[] args) {
 
--- vm-provider-movie/src/main/java/com/vm/ProviderMovieApplication.java
+++ vm-provider-movie/src/main/java/com/vm/ProviderMovieApplication.java
@@ -3,10 +3,12 @@ package com.vm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
 @ServletComponentScan
+@EnableDiscoveryClient
 public class ProviderMovieApplication {
     public static void main(String[] args) {
 
