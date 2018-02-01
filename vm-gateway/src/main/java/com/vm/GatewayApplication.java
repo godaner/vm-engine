1d77810deccafc3f354e0a210e27318d055bce10
zhangke <zhangke@wezyun.com>
2018-02-01 21:41:22 +0800

    gateway config

--- vm-gateway/src/main/java/com/vm/GatewayApplication.java
+++ vm-gateway/src/main/java/com/vm/GatewayApplication.java
@@ -12,7 +12,7 @@ import org.springframework.web.filter.CorsFilter;
 /**
  * Created by ZhangKe on 2017/12/26.
  */
-@EnableDiscoveryClient
+
 @SpringBootApplication
 @EnableZuulProxy // 使用@EnableZuulProxy来开启Zuul的支持，如果你不想使用Zuul提供的Filter和反向代理的功能的话，此处可以使用@EnableZuulServer注解
 public class GatewayApplication {
