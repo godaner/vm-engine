e62d14ddef10205b65fbd1dcc90d295a8ce39de3
zhangke <zhangke@wezyun.com>
2018-03-05 09:33:56 +0800

    添加config server

--- vm-config/src/main/java/com/vm/ConfigApplication.java
+++ vm-config/src/main/java/com/vm/ConfigApplication.java
@@ -2,18 +2,15 @@ package com.vm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by ZhangKe on 2018/2/3.
  */
-
 @SpringBootApplication
-@ServletComponentScan
+@EnableConfigServer
 @EnableDiscoveryClient
 public class ConfigApplication {
-
     public static void main(String[] args) {
 
         SpringApplication springApplication = new SpringApplication(ConfigApplication.class);
