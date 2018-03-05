98a5a4d04d75b68b38c08cd6d624c366b53f29fe
zhangke <zhangke@wezyun.com>
2018-03-05 13:42:52 +0800

    调整

--- vm-config-server/src/main/java/com/vm/ConfigApplication.java
+++ vm-config-server/src/main/java/com/vm/ConfigApplication.java
@@ -1,3 +1,5 @@
+package com.vm;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
