dc37d9d8fe991f4fc9a4f9a3f9979bff54cd5959
zhangke <zhangke@wezyun.com>
2018-03-16 18:20:05 +0800

    备份数据库g

--- vm-provider-user/src/main/java/com/vm/user/dao/po/VmUsers.java
+++ vm-provider-user/src/main/java/com/vm/user/dao/po/VmUsers.java
@@ -4,7 +4,7 @@ import com.vm.dao.util.BasePo;
 import com.vm.base.util.ByteConstantVar;
 
 public class VmUsers extends BasePo {
-    
+
     public final static String DEFAULT_IMG_URL = null;
 
     private String username;
