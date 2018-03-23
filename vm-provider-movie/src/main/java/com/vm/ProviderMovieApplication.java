8f2242a57a67bb8dfd69927534b4a52a12be2e5b
zhangke <zhangke@wezyun.com>
2018-03-23 18:08:24 +0800

    video上传

--- vm-provider-movie/src/main/java/com/vm/ProviderMovieApplication.java
+++ vm-provider-movie/src/main/java/com/vm/ProviderMovieApplication.java
@@ -2,9 +2,13 @@ package com.vm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.MultipartConfigElement;
 
 
 @SpringBootApplication
@@ -18,5 +22,13 @@ public class ProviderMovieApplication {
         springApplication.run(args);
 
     }
-
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //单个文件最大
+        factory.setMaxFileSize("10240KB"); //KB,MB
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize("102400KB");
+        return factory.createMultipartConfig();
+    }
 }
