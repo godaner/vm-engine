7022e161732c6fe9ddbc3d9da8c3a7ca2ceb221b
zhangke <zhangke@wezyun.com>
2018-03-27 21:06:30 +0800

    admin logs

--- vm-provider-admin/src/main/java/com/vm/admin/dao/qo/VmAdminLoginLogsQueryBean.java
+++ vm-provider-admin/src/main/java/com/vm/admin/dao/qo/VmAdminLoginLogsQueryBean.java
@@ -5,8 +5,17 @@ import com.vm.dao.util.BaseQueryBean;
 /**
  * Created by ZhangKe on 2018/3/27.
  */
-public class VmAdminLoginLogsQueryBean extends BaseQueryBean{
+public class VmAdminLoginLogsQueryBean extends BaseQueryBean {
     private Long adminId;
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
     public Long getAdminId() {
         return adminId;
