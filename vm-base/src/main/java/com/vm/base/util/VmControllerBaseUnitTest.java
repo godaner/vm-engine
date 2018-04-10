1367db9246c623096ef5e7a0d6106dc5acf13fbf
zhangke <zhangke@wezyun.com>
2018-04-10 16:34:57 +0800

    BaseUnitTest

--- vm-base/src/main/java/com/vm/base/util/VmControllerBaseUnitTest.java
+++ vm-base/src/main/java/com/vm/base/util/VmControllerBaseUnitTest.java
@@ -2,13 +2,21 @@ package com.vm.base.util;
 
 import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Map;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 /**
  * Created by ZhangKe on 2018/4/10.
@@ -17,6 +25,7 @@ import org.springframework.web.context.WebApplicationContext;
 @SpringBootTest
 @ActiveProfiles(profiles = "dev")
 public class VmControllerBaseUnitTest extends CommonUtil  {
+    private final static Logger logger = LoggerFactory.getLogger(VmControllerBaseUnitTest.class);
     //@Autowired
     protected MockMvc mockMvc;
 
@@ -31,5 +40,19 @@ public class VmControllerBaseUnitTest extends CommonUtil  {
         //单个类  拦截器无效
         // mockMvc = MockMvcBuilders.standaloneSteup(userController).build();
 
+    }
+    protected void p(String url,MultiValueMap<String, String> params) throws Exception {
+        mockMvc.perform(post(url).params(params));
+    }
+    protected void p(String url) throws Exception {
+        mockMvc.perform(post(url));
+    }
+    protected void g(String url,MultiValueMap<String, String> params) throws Exception {
+        Object res = mockMvc.perform(get(url).params(params)).andReturn();
+        logger.info("[Response url] is : {} ! ",res);
+    }
+    protected void g(String url) throws Exception {
+        Object res = mockMvc.perform(get(url)).andReturn();
+        logger.info("[Response url] is : {} ! ",res);
     }
 }
--- vm-provider-user/src/test/java/com/vm/user/test/controller/VmUsersControllerTest.java
+++ vm-provider-user/src/test/java/com/vm/user/test/controller/VmUsersControllerTest.java
@@ -2,9 +2,6 @@ package com.vm.user.test.controller;
 
 import com.vm.base.util.VmControllerBaseUnitTest;
 import org.junit.Test;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 /**
  * Created by ZhangKe on 2018/4/10.
@@ -14,11 +11,6 @@ public class VmUsersControllerTest extends VmControllerBaseUnitTest {
 
     @Test
     public void getUserBasicInfo() throws Exception {
-
-        RequestBuilder request = MockMvcRequestBuilders.get("/user/1");
-
-        MvcResult mvcResult = mockMvc.perform(request).andReturn();
-
-        System.out.println(obj2JSONStringWithoutByte(mvcResult));
+        g("/user/1");
     }
 }
