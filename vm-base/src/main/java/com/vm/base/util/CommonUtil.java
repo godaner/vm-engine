f004dffb509dcb2aee8312ca1b8fe5baa9d963c2
zhangke <zhangke@wezyun.com>
2018-04-14 00:00:55 +0800

    perfect

--- vm-base/src/main/java/com/vm/base/util/CommonUtil.java
+++ vm-base/src/main/java/com/vm/base/util/CommonUtil.java
@@ -36,7 +36,6 @@ public class CommonUtil {
     public final static String obj2SimpleJSONString(Object obj) {
 
         return CommonUtil.obj2JSONString(obj, Lists.newArrayList(Byte.class, Byte[].class, File.class, MultipartFile.class));
-//        return "";
     }
 
     public final static String obj2JSONString(Object obj, List<Class> refuseCls) {
